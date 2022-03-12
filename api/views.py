c6351d14da52df297b861cd9179620e35ed72242
Nick Anderson Azemar <newnick1996@gmail.com>
2022-03-12 17:40:50 -0500

    .

--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 
-from countries.models import Countries
-from countries.serializers import CountriesSerializer
+from api.models import Countries
+from api.serializers import CountriesSerializer
 from rest_framework.decorators import api_views
 # Create your views here.
 
@@ -14,7 +14,7 @@ def countries_list(request):
 
   if request.method == 'GET':
     countries = Countries.objects.all()
-    
+
     name = request.GET.get('name', None)
     if name is not None:
       countries = countries.filter(name__icontains=name)
@@ -27,7 +27,7 @@ def countries_list(request):
     if countries_serializer.is_valid():
       countries_serializer.save()
       return JsonResponse(countries_serializer.data, status = status.HTTP_201_CREATED)
-    
+
   return JsonResponse(countries_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
